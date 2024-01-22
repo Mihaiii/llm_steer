6280b228af034aa20551b62011697587de35112e
Mihai <mihai.chirculescu@yahoo.com>
2024-01-22 19:49:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='SteerLLM',
-    version='1.0.0',
-    description='Steer LLM responses towards a certain topic/subject or to enhance rule-play characteristics (ex: personas which are more funny) or to enhance response capabilities (ex: make it provide correct responses to tricky puzzles), using steering vectors by doing activation engineering',
+    name='llm-steer',
+    version='0.0.3',
+    description='Steer LLM responses towards a certain topic/subject and enhance response capabilities using activation engineering by adding steer vectors',
     author='Mihai Chirculescu',
     author_email='apropodemine@gmail.com',
-    py_modules=['steer_llm'],
-    url="https://github.com/Mihaiii/SteerLLM",
+    py_modules=['llm_steer'],
+    url="https://github.com/Mihaiii/LLMSteer",
     install_requires=[
-        transformers
+        'transformers'
     ],
 )
